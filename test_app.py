034b517d26f6614cddba085d1512dc486524e1eb
naufalfahrezy <51335399+naufalfahrezy@users.noreply.github.com>
2025-10-26 17:20:15 +0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,2 +1,8 @@
-def test_dummy():
-    assert 1 + 1 == 2
+# test_app.py
+from app import app
+
+def test_root_ok():
+    client = app.test_client()
+    resp = client.get('/')
+    assert resp.status_code == 200
+    assert "Halo dari Flask + Docker + Jenkins!" in resp.get_data(as_text=True)
